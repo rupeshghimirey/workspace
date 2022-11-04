1a47c7d0ac344380e22ac53e8d5efc36f6d90c53
Rupesh Ghimire <75717375+rupeshghimirey@users.noreply.github.com>
2022-11-04 17:07:24 -0500

    Update Bike.java
    ANOTHER CHANGES IN LINE 05

--- ZPracticeModel/src/vehicle/Bike.java
+++ ZPracticeModel/src/vehicle/Bike.java
@@ -2,6 +2,8 @@ package vehicle;
 
 // this is the changes made by somoone else
 // Someone else change the business logics
+//Someone else change the business logics
+
 public class Bike extends Vehicle{
 
     public Bike(String vin) {
